0665e8c32dcddf42ccdba595a993c2f16b5c46d9
Floris <531240@edu.rocmn.nl>
2021-01-25 19:56:41 +0100

    Added result page.

--- src/pages/ResultPage.js
+++ src/pages/ResultPage.js
@@ -1,12 +1,140 @@
 import React from "react";
 import { Text, View } from "react-native";
 
-export default function ResultPage({route}) {
-  console.log(route)
+import styled from "styled-components/native"
+
+export default function ResultPage({route, navigation}) {
+  function toStartPage() {
+    navigation.navigate("StartPage")
+  }
+
+  const XYPoint = 1; // Temporary
+
+  const FirstParty = route["params"]["top3Parties"][0];
+  const FirstPartyPercentage = (XYPoint / FirstParty["distance"] * 100).toFixed(0);
+  //const FirstPartyPercentage = 100 - FirstPartyCalc;
+
+  const SecondParty = route["params"]["top3Parties"][1];
+  const SecondPartyPercentage = (XYPoint / SecondParty["distance"] * 100).toFixed(0);
+  //const SecondPartyPercentage = 100 - SecondPartyCalc;
+
+  const ThirdParty = route["params"]["top3Parties"][2];
+  const ThirdPartyPercentage = (XYPoint / ThirdParty["distance"] * 100).toFixed(0);
+  //const ThirdPartyPercentage = 100 - ThirdPartyCalc;
+
+  console.log(route["params"]);
 
   return (
-    <View>
-      <Text>Result page</Text>
-    </View>
-  );
+    <Wrapper>
+      <Content>
+        <TitleContainer>
+          <Title>Uw resultaten van de test!</Title>
+        </TitleContainer>
+        <PartiesContainer>
+          <PartyContainer>
+            <PartyName>{FirstParty["politicParty"]}</PartyName>
+            <PartyPercentage>{FirstPartyPercentage}%</PartyPercentage>
+          </PartyContainer>
+          <PartyContainer>
+            <PartyName>{SecondParty["politicParty"]}</PartyName>
+            <PartyPercentage>{SecondPartyPercentage}%</PartyPercentage>
+          </PartyContainer>
+          <PartyContainer>
+            <PartyName>{ThirdParty["politicParty"]}</PartyName>
+            <PartyPercentage>{ThirdPartyPercentage}%</PartyPercentage>
+          </PartyContainer>
+        </PartiesContainer>
+        <ButtonContainer>
+          <ReturnButton onPress={() => toStartPage()}>
+            <ReturnButtonText>Terug naar start</ReturnButtonText>
+          </ReturnButton>
+        </ButtonContainer>
+      </Content>
+      <FooterContainer>
+        <FooterImage source={require("../../assets/background_image.jpg")} />
+      </FooterContainer>
+    </Wrapper>
+  )
 }
+
+
+const Wrapper = styled.View`
+  height: 100%;
+  background: #beecf9;
+  padding-top: 30px;
+`
+
+const Content = styled.View`
+  height: 50%;
+  justify-content: space-between;
+`
+
+const TitleContainer = styled.View`
+  margin-bottom: 15px;
+  margin-left: 10px;
+`
+
+const Title = styled.Text`
+  font-size: 30px;
+  color: #025c7e;
+  font-weight: bold;
+`
+
+const PartiesContainer = styled.View`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  flex-wrap: wrap;
+  flex-direction: column;
+  width: 100%;
+`
+
+const PartyContainer = styled.View`
+  margin-bottom: 15px;
+  display: flex;
+  align-items: center;
+`
+
+const PartyName = styled.Text`
+  font-size: 30px;
+  color: #025c7e;
+  font-weight: bold;
+`
+
+const PartyPercentage = styled.Text`
+  font-size: 25px;
+  color: #025c7e;
+`
+
+const ButtonContainer = styled.View`
+  display: flex;
+  align-items: center;
+`
+
+const ReturnButton = styled.TouchableHighlight`
+  width: 80%;
+  height: 48px;
+  background-color: rgb(2, 92, 126);
+  border-radius: 20px;
+  align-items: center;
+  justify-content: center;
+  margin-bottom: 20px;
+`
+
+const ReturnButtonText = styled.Text`
+  font-size: 22px;
+  color: white;
+  font-weight: bold;
+`
+
+const FooterContainer = styled.View`
+  position: absolute;
+  bottom: 0px;
+  width: 100%;
+  height: 300px;
+`
+
+const FooterImage = styled.Image`
+  width: 100%;
+  height: 100%;
+`
